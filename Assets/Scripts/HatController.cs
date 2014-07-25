6bfb9a0b2583e5757c4127814b78d5eee4545956
Alexandre Roba <alexandre.roba@arobanet.be>
2014-07-25 14:14:42 +0200

    Added the Bowlong ball prefab and the edge collider on the hat

---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatController : MonoBehaviour 
+{
+	public Camera cam;
+	private float maxWidth;
+	
+	void Start () 
+	{
+		if(cam==null)
+			cam = Camera.main;
+		Vector3 upperCorner = new Vector3(Screen.width,Screen.height,0.0f);
+		var targetWidth = cam.ScreenToWorldPoint(upperCorner);
+		float hatWidth = renderer.bounds.extents.x;
+		maxWidth = targetWidth.x-hatWidth;
+	
+	}
+	
+	void FixedUpdate () {
+		var rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+		var targetPosition = new Vector3(rawPosition.x,0.0f,0.0f);
+		float targetWidth = Mathf.Clamp (targetPosition.x,-maxWidth,maxWidth);
+		targetPosition = new Vector3(targetWidth,targetPosition.y,targetPosition.z);
+		rigidbody2D.MovePosition(targetPosition);
+	}
+}
